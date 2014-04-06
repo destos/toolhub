78687b9e1f93bf672b258f2b150f59ae230d7b51
Patrick Forringer <patrick@forringer.com>
2014-04-06 11:51:07 -0500

    remove groups app

--- groups/models.py
+++
@@ -1,6 +0,0 @@
-from django.db import models
-from django_extents.db.models import TimeStampedModel
-
-
-class LocalGroup(TimeStampedModel):
-	"""Handles relations to different users and their shared tools"""
