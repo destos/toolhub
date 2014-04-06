9f80ea4753f06cdf101a92e3521a1f67035a8a13
Patrick Forringer <patrick@forringer.com>
2014-04-06 11:53:02 -0500

    Classifying more account views
    fixing up base template a lil, it needs lots of love

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,8 +1,6 @@
-from class_based_auth_views.views import LoginView, LogoutView
 # from django.core.urlresolvers import reverse
 from django.conf.urls import url, patterns
 # from django.views.generic import TemplateView
-from accounts.forms import LoginForm
 
 from accounts import views
 
@@ -10,14 +8,9 @@ from accounts import views
 urlpatterns = patterns(
     'account.views',
     url(r'^account/$', views.AccountView.as_view(), name="account"),
-    # maybe change these with:
-    # https://github.com/django/django/blob/master/django/contrib/auth/forms.py
-    url(r'^login/$', LoginView.as_view(
-        form_class=LoginForm, template_name='accounts/login.jinja'),
-        name="login"),
-    url(r'^logout/$', LogoutView.as_view(), name="logout"),
-    url(r'^register/$',
-        views.RegistrationView.as_view(), name='register'),
+    url(r'^login/$', views.LoginView.as_view(), name="login"),
+    url(r'^logout/$', views.LogoutView.as_view(), name="logout"),
+    url(r'^register/$', views.RegistrationView.as_view(), name='register'),
     url(r'^user/(?P<slug>[-_\w]+)/$',
         views.UserDetailView.as_view(), name='user-detail')
 )
--- accounts/views.py
+++ accounts/views.py
@@ -1,16 +1,28 @@
+from class_based_auth_views.views import (
+    LoginView as ClassLoginView, LogoutView as ClassLogoutView)
 from django.contrib.auth.models import User
-# from django.core.urlresolvers import reverse
+from django.core.urlresolvers import reverse
 from django.views.generic import DetailView
 from django.views.generic.edit import FormView
 
-from accounts.forms import RegisterForm
+from accounts.forms import RegisterForm, LoginForm
+
+
+class LoginView(ClassLoginView):
+    form_class = LoginForm
+    template_name = 'accounts/login.jinja'
+
+
+class LogoutView(ClassLogoutView):
+    template_name = 'accounts/logout.jinja'
 
 
 # wanted to use CreateView, but that may not be the best use case
 class RegistrationView(FormView):
     form_class = RegisterForm
     template_name = 'accounts/register.jinja'
-    success_url = '/activate'  # reverse('accounts_activate')
+    # TODO: update
+    # success_url = reverse('account')
 
     def form_valid(self, form):
         # create user and send validation messages
@@ -23,4 +35,7 @@ class AccountView(DetailView):
 
 
 class UserDetailView(DetailView):
+    template_name = 'accounts/user_detail.jinja'
+    slug_field = 'username'
     model = User
+    context_object_name = 'user'
