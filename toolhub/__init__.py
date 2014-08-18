e52741cdf9b2cd8e71b7dc345ce01f9848b33e43
Patrick Forringer <patrick@forringer.com>
2014-08-17 21:16:06 -0500

    Adding javascript inline waffle flags

--- toolhub/__init__.py
+++ toolhub/__init__.py
@@ -3,6 +3,7 @@
 # Jinja filters and globals
 from django.conf import settings
 from waffle import flag_is_active, sample_is_active, switch_is_active
+from waffle.views import _generate_waffle_js
 import jinja2
 
 from .templatetags.customfilters import crispy, now
@@ -17,9 +18,15 @@ settings.JINJA2_GLOBALS['now'] = now
 def flag_helper(context, flag_name):
     return flag_is_active(context['request'], flag_name)
 
+
+@jinja2.contextfunction
+def inline_wafflejs_helper(context):
+    return _generate_waffle_js(context['request'])
+
+
 settings.JINJA2_GLOBALS['waffle'] = {
     'flag': flag_helper,
     'switch': switch_is_active,
     'sample': sample_is_active,
+    'wafflejs': inline_wafflejs_helper
 }
-
